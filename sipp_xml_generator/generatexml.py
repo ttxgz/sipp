23c2390433bb3e1d7b3e003483d4230ef96f1895
Zita Liao <ttxgzlwm@gmail.com>
2017-10-12 11:36:51 +1100

    good for reinvite loop

--- sipp_xml_generator/generatexml.py
+++ sipp_xml_generator/generatexml.py
@@ -32,6 +32,10 @@ def MakeCDATA(ele):
 	ele.append(cdata)
 	ele.text = ''
 
+def MakeCDATAAll(tree):
+	for send in tree.findall('send'):
+		MakeCDATA(send)
+
 def GenCreateConf(create_conf_msg, create_conf_tmplt, output_root):
 	params = ['cmd', 'conf_id','ri']
 	conf_id = FindParam(params, create_conf_msg, 'conf_id', 'conf_0')
@@ -108,43 +112,82 @@ def GenPLAY(play_msg, play_tmplt, output_root):
 	exec.attrib['rtp_stream'] = wav_file + ', ' + str(repeat) + ', ' + str(format)
 	AppendTag(play_tmplt, output_root)
 
-def GenINVITE(inv_msg, inv_tmplt, output_root):
-	params = ['cmd','audio_format','uri_user']
-	sends = inv_tmplt.findall('send')
-	send_sdp = sends[0]
-	# TODO: improve to a helper func to construct INVITE SDP
-	sdp_m = ''
-	sdp_media = []
-	sdp_m = FindParam(params, inv_tmplt,'audio_format','PCMA')
-	if (sdp_m == 'PCMU'):
-		sdp_m = 'm=audio [media_port] RTP/AVP 0'
-		sdp_media.append('a=rtpmap:0 PCMU/8000')
+def GenINVITE(inv_msg, inv_tmplt, output_root, sdp_less = False):
+	params = ['cmd','audio_format','log_id','uri_user']
+	# set sdp
+	sdp_media_id = []
+	sdp_media_map = []
+	audio_format = FindParam(params, inv_msg,'audio_format','PCMA')
+	if (audio_format == 'PCMU'):
+		sdp_media_id.append('0')
+		sdp_media_map.append('a=rtpmap:0 PCMU/8000')
+	# default: PCMA
 	else:
-		sdp_m = 'm=audio [media_port] RTP/AVP 8\n'
+		sdp_media_id.append('8')
 		sdp_media.append('a=rtpmap:8 PCMA/8000\n')
 
-	send_sdp.text = send_sdp.text.replace('[sdp_m]', sdp_m)
-	send_sdp.text = send_sdp.text.replace('[sdp_media]', '\n'.join(sdp_media))
+	sdp_m = inv_tmplt.find("nop/action/assignstr[@assign_to='sdp_m']")
+	sdp_m.attrib['value'] = ' '.join(sdp_media_id)
+	sdp_a = inv_tmplt.find("nop/action/assignstr[@assign_to='sdp_a']")
+	sdp_a.attrib['value'] = '\n'.join(sdp_media_map)
+	#set sdp to Invite or to Ack for sdp_less
+	if (sdp_less):
+		sdp_ack = inv_tmplt.find("nop/action/assignstr[@assign_to='sdp_ack']")
+		sdp_ack.attrib['value'] = "[$sdp]"
+	else:
+		sdp_invite = inv_tmplt.find("nop/action/assignstr[@assign_to='sdp_invite']")
+		sdp_invite.attrib['value'] = "[$sdp]"
 
+	#set uri_user
 	uri_user = FindParam(params, inv_msg, 'uri_user','sipp')
-	send_sdp.text = send_sdp.text.replace('[uri_user]', uri_user)
+	send_invite = inv_tmplt.find("send")
+	send_invite.text = send_invite.text.replace('[uri_user]', uri_user)
 
-	MakeCDATA(send_sdp)
+	#set log_id
+	log_id = FindParam(params, inv_msg, 'log_id','sipp_client')
+	send_invite.text = send_invite.text.replace('[log_id]', log_id)
 
-	# Convert CDATA in ACK
-	MakeCDATA(sends[1])
+	MakeCDATAAll(inv_tmplt)
 
 	AppendTag(inv_tmplt, output_root)
 
+def GenINVITEConfLeg(inv_leg_msg, inv_leg_tmplt, output_root):
+	params = ['cmd','log_id']
+	sends = inv_leg_tmplt.findall('send')
+	send_invite = sends[0]
+	log_id = FindParam(params, inv_leg_msg,'log_id','sipp_conf')
+	send_invite.text = send_invite.text.replace('[log_id]', log_id)
+
+	# Convert CDATA in all sends
+	MakeCDATAAll(inv_leg_tmplt)
+
+	AppendTag(inv_leg_tmplt, output_root)
+
 def GenBye(bye_msg, bye_tmplt, output_root):
 	params = ['cmd']
-	send = bye_tmplt.find('send')
-	MakeCDATA(send)
+	MakeCDATAAll(bye_tmplt)
 	AppendTag(bye_tmplt, output_root)
+
+def GenReInviteOrINFO(reinv_or_info_msg, reinv_or_info_tmplt, output_root):
+	params = ['cmd','loop_out_cnt']
+	# set loop out cnt
+	loop_cnt = int(FindParam(params, reinv_or_info_msg,'loop_out_cnt','0'))
+	if loop_cnt > 0:
+		loop_max = reinv_or_info_tmplt.find("nop/action/assign[@assign_to='reinvite_loop_max']")
+		loop_max.attrib['value'] = str(loop_cnt)
+		loop_increment = reinv_or_info_tmplt.find("nop/action/assign[@assign_to='reinvite_loop_increment']")
+		loop_increment.attrib['value'] = '1'
+
+	MakeCDATAAll(reinv_or_info_tmplt)
+	AppendTag(reinv_or_info_tmplt, output_root)
 
 def InsertXMLTag(sipp_msg, tmplt_root, output_root):
 	if (sipp_msg[0] == 'INVITE'):
 		GenINVITE(sipp_msg, copy.deepcopy(tmplt_root.find('invite')), output_root)
+	elif (sipp_msg[0] == 'INVITE_SDPLESS'):
+		GenINVITE(sipp_msg, copy.deepcopy(tmplt_root.find('invite')), output_root, True)
+	elif (sipp_msg[0] == 'INVITE_CONF'):
+		GenINVITEConfLeg(sipp_msg, copy.deepcopy(tmplt_root.find('invite_conf_leg')), output_root)
 	elif (sipp_msg[0] == 'PLAY'):
 		GenPLAY(sipp_msg, copy.deepcopy(tmplt_root.find('play')), output_root)
 	elif (sipp_msg[0] == 'CREATE_CONF'):
@@ -159,6 +202,8 @@ def InsertXMLTag(sipp_msg, tmplt_root, output_root):
 		GenWait(sipp_msg, copy.deepcopy(tmplt_root.find('wait')), output_root)
 	elif (sipp_msg[0] == 'BYE'):
 		GenBye(sipp_msg, copy.deepcopy(tmplt_root.find('bye')), output_root)
+	elif (sipp_msg[0] == 'INFO_OR_REINVITE'):
+		GenReInviteOrINFO(sipp_msg, copy.deepcopy(tmplt_root.find('reInvite_or_INFO')), output_root)
 	else:
 		print("Oops, not support " + sipp_msg[0] + " yet!")
 
